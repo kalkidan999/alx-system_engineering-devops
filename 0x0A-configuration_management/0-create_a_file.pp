ee506a40f353059bb51cc149c86aff7134807105
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-08-13 11:49:36 +0300

    fix soft-space

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
 #  create a file in /tmp using puppet
 file { '/tmp/holberton':
-   mode    => '0744',
-   owner   => 'www-data',
-   group   => 'www-data',
-   content => 'I love Puppet', }
+    ensure  => 'present',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet', }
