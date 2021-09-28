84aa262a06c1fd4880463978a405f38268a2c378
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-09-28 03:30:13 -0700

    Create 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+function that queries Reddit API and returns
+number of subscribers (not active users, total subscribers)
+"""
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ return number of subscribers """
+    url = requests.get("https://www.reddit.com/r/{}/about.json"
+                           .format(subreddit), headers={"User-Agent": "kalkidan"})
+    if url.status_code == 200:
+        return url.json().get("data").get("subscribers")
+    else:
+        return 0
